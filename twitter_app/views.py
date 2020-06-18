bc48c9f66b92bed4bb96150691cfbc2abd0e665d
dmitry <dmitry.filin@yahoo.com>
2020-06-18 22:47:07 +0400

    Add Json Response

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    print (args, kwargs)
     return HttpResponse("<h1>Hello Worlds </h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print (args, kwargs)
     try:
         obj = Tweet.objects.get(id=tweet_id)
     except:
         raise Http404
-    return HttpResponse(f"<h1>Tweet {tweet_id}: {obj.content} </h1>")
+    data = {
+        "id" : tweet_id,
+        "content" : obj.content,
+    }
+    return JsonResponse(data=data)
