d108fb431c75fe440f7ed7886f20bd84f8cf31ca
dmitry <dmitry.filin@yahoo.com>
2020-07-03 20:50:58 +0400

    Add checking on safe redirect

--- twitter/settings.py
+++ twitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%n+j398_-fzyl2!=dl(l4wmlw-6$rwal417!f)pk#if$%cej2='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
--- twitter_app/views.py
+++ twitter_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
+from django.utils.http import is_safe_url
+from django.conf import settings
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 from random import randint
 # Create your views here.
@@ -20,10 +24,13 @@ def tweet_list_view (request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None) # When the QueryDict request.POST is empty, it takes a Falsy value
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False) #to modify obj later and not dump it right now
         # do other form related logic
         obj.save()
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm() # to return blank form after request handle
     return render(request=request, template_name="components/form.html", context={"form": form})
 
